97095bb3781b2abd68f10c7ce572540b342182d8
Adarsh Gajbhare <107011378+adarshgajbhare@users.noreply.github.com>
2026-09-26 13:24:46 +0530

    Time: 5 ms (84.05%) | Memory: 46 MB (52.64%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -6,7 +6,7 @@ class Solution {
         }
         int original = x;
         int rev = 0;
-        while (x > 0) {
+        while (x != 0) {
             int rem = x % 10;
             rev = rev * 10 + rem;
             x = x / 10;
